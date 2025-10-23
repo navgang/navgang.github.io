f9b9559ecda81969eadee9840ff9b915260f793f
navgang <98491437+navgang@users.noreply.github.com>
2025-10-23 11:06:26 -0700

    folder to deploy website

---
+++ dist/_astro/index.astro_astro_type_script_index_0_lang.CkSin5h3.js
@@ -0,0 +1,1 @@
+import{l as a}from"./constants.CbTSZZC9.js";import{a as o}from"./index.DHyBpiuH.js";const m=[a];o(m);
---
+++ dist/_astro/travel.astro_astro_type_script_index_0_lang.CGEPihAM.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.DHyBpiuH.js";const n=[[".loaderRef",{opacity:[1,0],pointerEvents:"none"},{type:"spring",easing:"ease-out"}]];e(n);
